82e6fe85d61379faec45fb329b3832b924717b85
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2015-12-24 04:37:48 +0000

    Set up hirs_ctp_orbital to use v2 CSRB files, and added local execution script.

--- source/flo/__init__.py
+++ source/flo/__init__.py
@@ -1,12 +1,16 @@
 
 from datetime import datetime, timedelta
 from glob import glob
-import os
+import os,sys
 import shutil
 from flo.computation import Computation
 from flo.time import round_datetime
 from flo.subprocess import check_call
 from flo.util import augmented_env, symlink_inputs_to_working_dir
+from flo.config import config
+from flo.product import StoredProductCatalog
+from flo.ingest import IngestCatalog
+
 from flo.sw.hirs import HIRS
 from flo.sw.hirs.util import generate_cfsr_bin
 from flo.sw.hirs_avhrr import HIRS_AVHRR
@@ -17,6 +21,9 @@ from flo.sw.hirs.delta import delta_catalog
 import logging, traceback
 LOG = logging.getLogger(__file__)
 
+ingest_catalog = IngestCatalog('PEATE')
+SPC = StoredProductCatalog()
+
 
 class HIRS_CTP_ORBITAL(Computation):
 
@@ -24,32 +31,57 @@ class HIRS_CTP_ORBITAL(Computation):
     outputs = ['out']
 
     def build_task(self, context, task):
+        LOG.debug("Running build_task()") # GPC
+        LOG.debug("context:  {}".format(context)) # GPC
+        LOG.debug("Initial task.inputs:  {}".format(task.inputs)) # GPC
 
         # HIRS L1B Input
         hirs_context = context.copy()
         [hirs_context.pop(k) for k in ['collo_version', 'csrb_version', 'ctp_version']]
+        LOG.debug("hirs_context:  {}".format(hirs_context)) # GPC
         task.input('HIR1B', HIRS().dataset('out').product(hirs_context))
+
 
         # Collo Input
         collo_context = hirs_context
         collo_context['collo_version'] = context['collo_version']
+        LOG.debug("collo_context:  {}".format(collo_context)) # GPC
         task.input('COLLO', HIRS_AVHRR().dataset('out').product(collo_context))
 
         # CSRB Monthly Input
         csrb_context = collo_context
         csrb_context['csrb_version'] = context['csrb_version']
         csrb_context['granule'] = datetime(context['granule'].year, context['granule'].month, 1)
+        LOG.debug("csrb_context:  {}".format(csrb_context)) # GPC
         task.input('CSRB', HIRS_CSRB_MONTHLY().dataset('zonal_means').product(csrb_context))
 
         # PTMSX Input
-        task.input('PTMSX', delta_catalog.file('avhrr', context['sat'], 'PTMSX',
-                                               context['granule']))
+        LOG.debug("Searching for PTMSX file for granule  {}".format(context['granule'])) # GPC
+        ptmsx_files = delta_catalog.file('avhrr', context['sat'],'PTMSX', context['granule'])
+        task.input('PTMSX',ptmsx_files)
 
         # CFSR Input
         cfsr_granule = round_datetime(context['granule'], timedelta(hours=6))
-        task.input('CFSR', delta_catalog.file('ancillary', 'NONE', 'CFSR', cfsr_granule))
+        LOG.debug("cfsr_granule:  {}".format(cfsr_granule)) # GPC
+        cfsr_file = self.get_cfsr(cfsr_granule)
+        task.input('CFSR', cfsr_file)
+
+        LOG.debug("Final task.inputs...") # GPC
+        for task_key in task.inputs.keys():
+            LOG.debug("\t{}: {}".format(task_key,task.inputs[task_key])) # GPC
+
+        for task_key in ['HIR1B','COLLO','CSRB']:
+            if task_key in task.inputs.keys():
+                LOG.debug("{} file: {}".format(task_key,SPC.file(task.inputs[task_key]).path))
+        for task_key in ['CFSR']:
+            if task_key in task.inputs.keys():
+                LOG.debug("{} file: {}".format(task_key,task.inputs[task_key].path))
+
+        LOG.debug("Exiting build_task()...") # GPC
+
 
     def run_task(self, inputs, context):
+        LOG.debug("Running run_task()") # GPC
 
         debug = 0
         shifted_FM_opt = 2
@@ -57,6 +89,8 @@ class HIRS_CTP_ORBITAL(Computation):
         # Inputs
         inputs = symlink_inputs_to_working_dir(inputs)
         lib_dir = os.path.join(self.package_root, context['ctp_version'], 'lib')
+
+        LOG.debug("inputs :  {}".format(inputs)) # GPC
 
         # Output Name
         output = 'ctp.orbital.{}.{}.nc'.format(context['sat'], inputs['HIR1B'][12:30])
@@ -83,9 +117,59 @@ class HIRS_CTP_ORBITAL(Computation):
 
         return {'out': output}
 
+
+    def get_cfsr(self,cfsr_granule):
+
+        num_cfsr_files = 0
+
+        # Search for the old style pgbhnl.gdas.*.grb2 file from the PEATE
+        if num_cfsr_files == 0:
+            LOG.debug("Trying to retrieve pgbhnl.gdas.*.grb2 CFSR files from PEATE...") # GPC
+            try:
+                cfsr_file = ingest_catalog.file('CFSR_PGRBHANL',cfsr_granule)
+                num_cfsr_files = len(cfsr_file)
+                if num_cfsr_files != 0:
+                    LOG.debug("\tpgbhnl.gdas.*.grb2 CFSR files from PEATE : {}".format(cfsr_file)) # GPC
+            except Exception, err :
+                LOG.error("{}.".format(err))
+                LOG.warn("\tRetrieval of pgbhnl.gdas.*.grb2 CFSR file from PEATE failed") # GPC
+
+        # Search for the new style cdas1.*.t*z.pgrbhanl.grib2 file from PEATE
+        #num_cfsr_files = 0
+        if num_cfsr_files == 0:
+            LOG.debug("Trying to retrieve cdas1.*.t*z.pgrbhanl.grib2 CFSR file from PEATE...") # GPC
+            try:
+                cfsr_file = ingest_catalog.file('CFSV2_PGRBHANL',cfsr_granule)
+                num_cfsr_files = len(cfsr_file)
+                if num_cfsr_files != 0:
+                    LOG.debug("\tcdas1.*.t*z.pgrbhanl.grib2 CFSR file from PEATE : {}".format(cfsr_file)) # GPC
+            except Exception, err :
+                LOG.error("{}.".format(err))
+                LOG.warn("\tRetrieval of cdas1.*.t*z.pgrbhanl.grib2 CFSR file from PEATE failed") # GPC
+
+        # Search for the old style pgbhnl.gdas.*.grb2 file from the file list
+        #num_cfsr_files = 0
+        #if num_cfsr_files == 0:
+            #LOG.debug("Trying to retrieve pgbhnl.gdas.*.grb2 CFSR file from DELTA...") # GPC
+            #try:
+                #cfsr_file = delta_catalog.file('ancillary', 'NONE', 'CFSR', cfsr_granule)
+                #num_cfsr_files = len(cfsr_file)
+                #if num_cfsr_files != 0:
+                    #LOG.debug("pgbhnl.gdas.*.grb2 CFSR file from DELTA : {}\n".format(cfsr_file)) # GPC
+            #except Exception, err :
+                #LOG.error("{}.".format(err))
+                #LOG.warn("\tRetrieval of pgbhnl.gdas.*.grb2 CFSR file from DELTA failed\n") # GPC
+
+
+        #LOG.info("We've found {} CFSR file for context {}".format(len(cfsr_file),cfsr_granule)) # GPC
+
+        return cfsr_file
+
+
     def find_contexts(self, sat, hirs_version, collo_version, csrb_version, ctp_version,
                       time_interval):
 
+        LOG.debug("Running find_contexts()") # GPC
         files = delta_catalog.files('hirs', sat, 'HIR1B', time_interval)
         return [{'granule': file.data_interval.left,
                  'sat': sat,
@@ -98,6 +182,7 @@ class HIRS_CTP_ORBITAL(Computation):
 
     def context_path(self, context, output):
 
+        LOG.debug("Running context_path()") # GPC
         return os.path.join('HIRS',
                             '{}/{}'.format(context['sat'], context['granule'].year),
                             'CTP_ORBITAL')
