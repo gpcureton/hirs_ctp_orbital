d83c48458fffdfc8a20c1b42662bfa9da9ae36dc
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2018-08-01 01:26:35 +0000

    Updated hirs_ctp_orbital to use software delivery provided by sips_deliver.

--- example_local_prepare.py
+++ example_local_prepare.py
@@ -15,20 +15,25 @@ import logging
 from timeutil import TimeInterval, datetime, timedelta
 from flo.ui import local_prepare, local_execute
 
-import flo.sw.hirs as hirs
+import flo.sw.hirs2nc as hirs2nc
 import flo.sw.hirs_avhrr as hirs_avhrr
 import flo.sw.hirs_csrb_monthly as hirs_csrb_monthly
 import flo.sw.hirs_ctp_orbital as hirs_ctp_orbital
-from flo.sw.hirs.utils import setup_logging
+
+from flo.sw.hirs2nc.utils import setup_logging
 
 # every module should have a LOG object
 LOG = logging.getLogger(__name__)
 
+#
 # General information
-hirs_version  = 'v20151014'
-collo_version = 'v20151014'
-csrb_version  = 'v20150915'
-ctp_version = 'v20150915'
+#
+
+#hirs2nc_delivery_id = '20180410-1'
+#hirs_avhrr_delivery_id = '20180505-1'
+#hirs_csrb_daily_delivery_id  = '20180714-1'
+#hirs_csrb_monthly_delivery_id  = '20180516-1'
+#hirs_ctp_orbital_delivery_id  = '20180730-1'
 wedge = timedelta(seconds=1.)
 
 # Satellite specific information
@@ -36,45 +41,48 @@ wedge = timedelta(seconds=1.)
 #granule = datetime(2017, 1, 1, 0, 32)
 #interval = TimeInterval(granule, granule+timedelta(seconds=0))
 
-# Data locations
-collection = {'HIR1B': 'ILIAD',
-              'CFSR': 'DELTA',
-              'PTMSX': 'ILIAD'}
-# NOAA-19
-#satellite = 'noaa-19'
-#input_data = {'HIR1B': '/mnt/sdata/geoffc/HIRS_processing/data_lists/NOAA-19/HIR1B_noaa-19_latest',
-              #'CFSR':  '/mnt/sdata/geoffc/HIRS_processing/data_lists/CFSR.out',
-              #'PTMSX': '/mnt/sdata/geoffc/HIRS_processing/data_lists/NOAA-19/PTMSX_noaa-19_latest'}
+def setup_computation(satellite):
 
-# Metop-B
-#satellite = 'metop-b'
-input_data = {'HIR1B': '/mnt/cephfs_data/geoffc/hirs_data_lists/Metop-B/HIR1B_metop-b_latest',
-              'CFSR':  '/mnt/cephfs_data/geoffc/hirs_data_lists/CFSR.out',
-              'PTMSX': '/mnt/cephfs_data/geoffc/hirs_data_lists/Metop-B/PTMSX_metop-b_latest'}
+    input_data = {'HIR1B': '/mnt/software/flo/hirs_l1b_datalists/{0:}/HIR1B_{0:}_latest'.format(satellite),
+                  'CFSR':  '/mnt/cephfs_data/geoffc/hirs_data_lists/CFSR.out',
+                  'PTMSX': '/mnt/software/flo/hirs_l1b_datalists/{0:}/PTMSX_{0:}_latest'.format(satellite)}
 
-input_sources = {'collection':collection, 'input_data':input_data}
+    # Data locations
+    collection = {'HIR1B': 'ILIAD',
+                  'CFSR': 'DELTA',
+                  'PTMSX': 'FJORD'}
 
-# Initialize the hirs_avhrr module with the data locations
-hirs_ctp_orbital.set_input_sources(input_sources)
+    input_sources = {'collection':collection, 'input_data':input_data}
 
-# Instantiate the computations
-hirs_comp = hirs.HIRS()
-hirs_avhrr_comp = hirs_avhrr.HIRS_AVHRR()
-hirs_csrb_monthly_comp = hirs_csrb_monthly.HIRS_CSRB_MONTHLY()
-comp = hirs_ctp_orbital.HIRS_CTP_ORBITAL()
+    # Initialize the hirs_ctp_orbital module with the data locations
+    hirs_ctp_orbital.set_input_sources(input_sources)
+
+    # Instantiate the computation
+    comp = hirs_ctp_orbital.HIRS_CTP_ORBITAL()
+
+    return comp
+
 
 #
 # Local execution
 #
 
-def local_execute_example(interval, satellite, hirs_version, collo_version, csrb_version, ctp_version,
+def local_execute_example(interval, satellite, hirs2nc_delivery_id, hirs_avhrr_delivery_id,
+                          hirs_csrb_daily_delivery_id, hirs_csrb_monthly_delivery_id,
+                          hirs_ctp_orbital_delivery_id,
                           skip_prepare=False, skip_execute=False, verbosity=2):
-
 
     setup_logging(verbosity)
 
+    comp = setup_computation(satellite)
+    hirs2nc_comp = hirs2nc.HIRS2NC()
+    hirs_avhrr_comp = hirs_avhrr.HIRS_AVHRR()
+    hirs_csrb_monthly_comp = hirs_csrb_monthly.HIRS_CSRB_MONTHLY()
+
     # Get the required context...
-    contexts = comp.find_contexts(interval, satellite, hirs_version, collo_version, csrb_version, ctp_version)
+    contexts = comp.find_contexts(interval, satellite, hirs2nc_delivery_id, hirs_avhrr_delivery_id,
+                                  hirs_csrb_daily_delivery_id, hirs_csrb_monthly_delivery_id,
+                                  hirs_ctp_orbital_delivery_id)
 
     if len(contexts) != 0:
         LOG.info("Candidate contexts in interval...")
@@ -85,7 +93,7 @@ def local_execute_example(interval, satellite, hirs_version, collo_version, csrb
             if not skip_prepare:
                 LOG.info("Running hirs_ctp_orbital local_prepare()...")
                 LOG.info("Preparing context... {}".format(contexts[0]))
-                local_prepare(comp, contexts[0],download_only=[hirs_comp, hirs_avhrr_comp, hirs_csrb_monthly_comp])
+                local_prepare(comp, contexts[0], download_only=[hirs2nc_comp, hirs_avhrr_comp, hirs_csrb_monthly_comp])
             if not skip_execute:
                 LOG.info("Running hirs_ctp_orbital local_execute()...")
                 LOG.info("Running context... {}".format(contexts[0]))
@@ -96,14 +104,21 @@ def local_execute_example(interval, satellite, hirs_version, collo_version, csrb
     else:
         LOG.error("There are no valid {} contexts for the interval {}.".format(satellite, interval))
 
-def print_contexts(interval, satellite, hirs_version, collo_version, csrb_version, ctp_version, verbosity=2):
+def print_contexts(interval, satellite, hirs2nc_delivery_id, hirs_avhrr_delivery_id,
+                   hirs_csrb_daily_delivery_id, hirs_csrb_monthly_delivery_id,
+                   hirs_ctp_orbital_delivery_id, verbosity=2):
+
     setup_logging(verbosity)
-    contexts = comp.find_contexts(interval, satellite, hirs_version, collo_version, csrb_version, ctp_version)
+
+    comp = setup_computation(satellite)
+
+    contexts = comp.find_contexts(interval, satellite, hirs2nc_delivery_id, hirs_avhrr_delivery_id,
+                                  hirs_csrb_daily_delivery_id, hirs_csrb_monthly_delivery_id,
+                                  hirs_ctp_orbital_delivery_id)
+    LOG.info('Printing contexts...')
     for context in contexts:
         LOG.info(context)
 
 #satellite_choices = ['noaa-06', 'noaa-07', 'noaa-08', 'noaa-09', 'noaa-10', 'noaa-11',
                     #'noaa-12', 'noaa-14', 'noaa-15', 'noaa-16', 'noaa-17', 'noaa-18',
                     #'noaa-19', 'metop-a', 'metop-b']
-
-#local_execute_example(granule, satellite, hirs_version, collo_version, csrb_version, ctp_version)
--- source/flo/__init__.py
+++ source/flo/__init__.py
@@ -15,6 +15,7 @@ from glob import glob
 import shutil
 import logging
 import traceback
+from subprocess import CalledProcessError
 
 from flo.computation import Computation
 from flo.builder import WorkflowNotReady
@@ -26,9 +27,9 @@ import sipsprod
 from glutil import (
     check_call,
     dawg_catalog,
-    #delivered_software,
+    delivered_software,
     #support_software,
-    #runscript,
+    runscript,
     #prepare_env,
     #nc_gen,
     nc_compress,
@@ -36,15 +37,14 @@ from glutil import (
     #set_official_product_metadata,
     FileNotFound
 )
-import flo.sw.hirs as hirs
+import flo.sw.hirs2nc as hirs2nc
 import flo.sw.hirs_avhrr as hirs_avhrr
 import flo.sw.hirs_csrb_monthly as hirs_csrb_monthly
-from flo.sw.hirs.delta import DeltaCatalog
+from flo.sw.hirs2nc.delta import DeltaCatalog
+from flo.sw.hirs2nc.utils import link_files
 
 # every module should have a LOG object
 LOG = logging.getLogger(__name__)
-
-SPC = StoredProductCatalog()
 
 def set_input_sources(input_locations):
     global delta_catalog
@@ -52,126 +52,26 @@ def set_input_sources(input_locations):
 
 class HIRS_CTP_ORBITAL(Computation):
 
-    parameters = ['granule', 'sat', 'hirs_version', 'collo_version', 'csrb_version', 'ctp_version']
+    parameters = ['granule', 'satellite', 'hirs2nc_delivery_id', 'hirs_avhrr_delivery_id',
+                  'hirs_csrb_daily_delivery_id', 'hirs_csrb_monthly_delivery_id',
+                  'hirs_ctp_orbital_delivery_id']
     outputs = ['out']
 
-    @reraise_as(WorkflowNotReady, FileNotFound, prefix='HIRS_CTP_ORBITAL')
-    def build_task(self, context, task):
-        '''
-        Build up a set of inputs for a single context
-        '''
-        global delta_catalog
+    def find_contexts(self, time_interval, satellite, hirs2nc_delivery_id, hirs_avhrr_delivery_id,
+                      hirs_csrb_daily_delivery_id, hirs_csrb_monthly_delivery_id,
+                      hirs_ctp_orbital_delivery_id):
 
-        LOG.debug("Running build_task()")
-        LOG.debug("context:  {}".format(context))
-
-        # Initialize the hirs and hirs_avhrr modules with the data locations
-        hirs.delta_catalog = delta_catalog
-        hirs_avhrr.delta_catalog = delta_catalog
-
-        # Instantiate the hirs, hirs_avhrr and hirs_csrb_monthly computations
-        hirs_comp = hirs.HIRS()
-        hirs_avhrr_comp = hirs_avhrr.HIRS_AVHRR()
-        hirs_csrb_monthly_comp = hirs_csrb_monthly.HIRS_CSRB_MONTHLY()
-
-        # HIRS L1B Input
-        hirs_context = context.copy()
-        [hirs_context.pop(k) for k in ['collo_version', 'csrb_version', 'ctp_version']]
-        LOG.debug("hirs_context:  {}".format(hirs_context))
-        hirs_prod = hirs_comp.dataset('out').product(hirs_context)
-
-        if SPC.exists(hirs_prod):
-            task.input('HIR1B', hirs_prod)
-        else:
-            raise WorkflowNotReady('No HIRS inputs available for {}'.format(hirs_context['granule']))
-
-        # Collo Input
-        hirs_avhrr_context = hirs_context
-        hirs_avhrr_context['collo_version'] = context['collo_version']
-        LOG.debug("hirs_avhrr_context:  {}".format(hirs_avhrr_context))
-        hirs_avhrr_prod = hirs_avhrr_comp.dataset('out').product(hirs_avhrr_context)
-
-        if SPC.exists(hirs_avhrr_prod):
-            task.input('COLLO', hirs_avhrr_prod)
-        else:
-            raise WorkflowNotReady('No HIRS_AVHRR inputs available for {}'.format(hirs_avhrr_context['granule']))
-
-        # CSRB Monthly Input
-        csrb_context = hirs_avhrr_context
-        csrb_context['csrb_version'] = context['csrb_version']
-        csrb_context['granule'] = datetime(context['granule'].year, context['granule'].month, 1)
-        LOG.debug("csrb_context:  {}".format(csrb_context))
-        hirs_csrb_monthly_prod = hirs_csrb_monthly_comp.dataset('zonal_means').product(csrb_context)
-
-        if SPC.exists(hirs_csrb_monthly_prod):
-            task.input('CSRB', hirs_csrb_monthly_prod)
-        else:
-            raise WorkflowNotReady('No HIRS_CSRB_MONTHLY inputs available for {}'.format(csrb_context['granule']))
-
-        # PTMSX Input
-        LOG.debug('Getting PTMSX input...')
-        sensor = 'avhrr'
-        sat =  context['sat']
-        file_type = 'PTMSX'
-        granule = context['granule']
-
-        try:
-            ptmsx_file = delta_catalog.file(sensor, sat, file_type, granule)
-            task.input('PTMSX',ptmsx_file)
-        except IngestFileMissing:
-            raise WorkflowNotReady('No PTMSX inputs available for {}'.format(granule))
-
-        # CFSR Input
-        LOG.debug('Getting CFSR input...')
-        cfsr_file = self.get_cfsr(context['granule'])
-        if cfsr_file is not None:
-            task.input('CFSR', cfsr_file)
-        else:
-            raise WorkflowNotReady('No CFSR inputs available for {}'.format(granule))
-
-        LOG.debug("Final task.inputs...")
-        for task_key in task.inputs.keys():
-            LOG.debug("\t{}: {}".format(task_key,task.inputs[task_key]))
-
-    @reraise_as(WorkflowNotReady, FileNotFound, prefix='HIRS_CTP_ORBITAL')
-    def run_task(self, inputs, context):
-        LOG.debug("Running run_task()")
-
-        debug = 0
-        shifted_FM_opt = 2
-
-        # Inputs
-        inputs = symlink_inputs_to_working_dir(inputs)
-        lib_dir = os.path.join(self.package_root, context['ctp_version'], 'lib')
-
-        LOG.debug("inputs :  {}".format(inputs))
-
-        # Output Name
-        output = 'ctp.orbital.{}.{}.nc'.format(context['sat'], inputs['HIR1B'][12:30])
-        LOG.debug("output :  {}".format(output))
-
-        # Copy coeffs to working directory
-        [shutil.copy(f, './')
-         for f
-         in glob(os.path.join(self.package_root, context['ctp_version'], 'coeffs/*'))]
-
-        # Generating CFSR Binaries
-        cfsr_bin_files = self.generate_cfsr_bin(os.path.join(self.package_root, context['ctp_version']))
-        LOG.debug("cfsr_bin_files :  {}".format(cfsr_bin_files))
-
-        # Running CTP Orbital
-        cmd = os.path.join(self.package_root, context['ctp_version'],
-                           'bin/process_hirs_cfsr.exe')
-        cmd += ' {} {}.bin {}'.format(inputs['HIR1B'], inputs['CFSR'], inputs['COLLO'])
-        cmd += ' {} {}'.format(inputs['PTMSX'], inputs['CSRB'])
-        cmd += ' {}'.format(os.path.join(self.package_root, context['ctp_version'],
-                                         'CFSR_lst.bin'))
-        cmd += ' {} {} {}'.format(debug, shifted_FM_opt, output)
-
-        LOG.debug(cmd)
-        check_call(cmd, shell=True, env=augmented_env({'LD_LIBRARY_PATH': lib_dir}))
-
-        return {'out': output}
+        LOG.debug("Running find_contexts()")
+        files = delta_catalog.files('hirs', satellite, 'HIR1B', time_interval)
+        return [{'granule': file.data_interval.left,
+                 'satellite': satellite,
+                 'hirs2nc_delivery_id': hirs2nc_delivery_id,
+                 'hirs_avhrr_delivery_id': hirs_avhrr_delivery_id,
+                 'hirs_csrb_daily_delivery_id': hirs_csrb_daily_delivery_id,
+                 'hirs_csrb_monthly_delivery_id': hirs_csrb_monthly_delivery_id,
+                 'hirs_ctp_orbital_delivery_id': hirs_ctp_orbital_delivery_id}
+                for file in files
+                if file.data_interval.left >= time_interval.left]
 
     def get_cfsr(self, granule):
         '''
@@ -206,58 +106,246 @@ class HIRS_CTP_ORBITAL(Computation):
 
         return cfsr_file
 
-    def generate_cfsr_bin(self,package_root):
+    @reraise_as(WorkflowNotReady, FileNotFound, prefix='HIRS_CTP_ORBITAL')
+    def build_task(self, context, task):
         '''
-        Convert the CFSR file into a flat binary file.
+        Build up a set of inputs for a single context
+        '''
+        global delta_catalog
+
+        LOG.debug("Running build_task()")
+
+        # Initialize the hirs2nc and hirs_avhrr modules with the data locations
+        hirs2nc.delta_catalog = delta_catalog
+        hirs_avhrr.delta_catalog = delta_catalog
+
+        # Instantiate the hirs, hirs_avhrr and hirs_csrb_monthly computations
+        hirs2nc_comp = hirs2nc.HIRS2NC()
+        hirs_avhrr_comp = hirs_avhrr.HIRS_AVHRR()
+        hirs_csrb_monthly_comp = hirs_csrb_monthly.HIRS_CSRB_MONTHLY()
+
+        SPC = StoredProductCatalog()
+
+        # HIRS L1B Input
+        hirs2nc_context = context.copy()
+        [hirs2nc_context.pop(k) for k in ['hirs_avhrr_delivery_id', 'hirs_csrb_daily_delivery_id',
+                                          'hirs_csrb_monthly_delivery_id', 'hirs_ctp_orbital_delivery_id']]
+        hirs2nc_prod = hirs2nc_comp.dataset('out').product(hirs2nc_context)
+
+        if SPC.exists(hirs2nc_prod):
+            task.input('HIR1B', hirs2nc_prod)
+        else:
+            raise WorkflowNotReady('No HIRS inputs available for {}'.format(hirs2nc_context['granule']))
+
+        # PTMSX Input
+        LOG.debug('Getting PTMSX input...')
+        sensor = 'avhrr'
+        satellite =  context['satellite']
+        file_type = 'PTMSX'
+        granule = context['granule']
+
+        try:
+            ptmsx_file = delta_catalog.file(sensor, satellite, file_type, granule)
+            task.input('PTMSX',ptmsx_file)
+        except IngestFileMissing:
+            raise WorkflowNotReady('No PTMSX inputs available for {}'.format(granule))
+
+        # Collo Input
+        hirs_avhrr_context = hirs2nc_context
+        hirs_avhrr_context['hirs_avhrr_delivery_id'] = context['hirs_avhrr_delivery_id']
+        hirs_avhrr_prod = hirs_avhrr_comp.dataset('out').product(hirs_avhrr_context)
+
+        if SPC.exists(hirs_avhrr_prod):
+            task.input('COLLO', hirs_avhrr_prod)
+        else:
+            raise WorkflowNotReady('No HIRS_AVHRR inputs available for {}'.format(hirs_avhrr_context['granule']))
+
+        # CSRB Monthly Input
+        hirs_csrb_monthly_context = context.copy()
+        [hirs_csrb_monthly_context.pop(k) for k in ['hirs_ctp_orbital_delivery_id']]
+        hirs_csrb_monthly_context['granule'] = datetime(context['granule'].year, context['granule'].month, 1)
+        hirs_csrb_monthly_prod = hirs_csrb_monthly_comp.dataset('zonal_means').product(hirs_csrb_monthly_context)
+
+        if SPC.exists(hirs_csrb_monthly_prod):
+            task.input('CSRB', hirs_csrb_monthly_prod)
+        else:
+            raise WorkflowNotReady('No HIRS_CSRB_MONTHLY inputs available for {}'.format(hirs_csrb_monthly_context['granule']))
+        # CFSR Input
+        LOG.debug('Getting CFSR input...')
+        cfsr_file = self.get_cfsr(context['granule'])
+        if cfsr_file is not None:
+            task.input('CFSR', cfsr_file)
+        else:
+            raise WorkflowNotReady('No CFSR inputs available for {}'.format(granule))
+
+        LOG.debug("Final task.inputs...")
+        for task_key in task.inputs.keys():
+            LOG.debug("\t{}: {}".format(task_key,task.inputs[task_key]))
+
+    def extract_bin_from_cfsr(self, inputs, context):
+        '''
+        Run wgrib2 on the  input CFSR grib files, to create flat binary files
+        containing the desired data.
         '''
 
-        shutil.copy(os.path.join(package_root, 'bin/wgrib2'), './')
+        # Where are we running the package
+        work_dir = abspath(curdir)
+        LOG.debug("working dir = {}".format(work_dir))
 
-        # Search for the old style pgbhnl.gdas.*.grb2 files
-        LOG.debug("Searching for pgbhnl.gdas.*.grb2 ...")
-        files = glob('pgbhnl.gdas.*.grb2')
-        LOG.debug("... found {}".format(files))
+        # Get the required CFSR and wgrib2 script locations
+        hirs_ctp_orbital_delivery_id = context['hirs_ctp_orbital_delivery_id']
+        delivery = delivered_software.lookup('hirs_ctp_orbital', delivery_id=hirs_ctp_orbital_delivery_id)
+        dist_root = pjoin(delivery.path, 'dist')
+        extract_cfsr_bin = pjoin(dist_root, 'bin/extract_cfsr.csh')
+        version = delivery.version
 
-        # Search for the new style cdas1.*.t*z.pgrbhanl.grib2
-        if len(files)==0:
-            LOG.debug("Searching for cdas1.*.pgrbhanl.grib2 ...")
-            files = glob('cdas1.*.pgrbhanl.grib2')
-            LOG.debug("... found {}".format(files))
+        # Get the CFSR input
+        cfsr_file = inputs['CFSR']
+        LOG.debug("CFSR file: {}".format(cfsr_file))
 
-        LOG.debug("CFSR files: {}".format(files))
-
+        # Extract the desired datasets for each CFSR file
+        rc = 0
         new_cfsr_files = []
-        for file in files:
-            cmd = os.path.join(package_root, 'bin/extract_cfsr.csh')
-            cmd += ' {} {}.bin ./'.format(file, file)
 
-            LOG.debug(cmd)
+        output_cfsr_file = '{}.bin'.format(basename(cfsr_file))
+        cmd = '{} {} {} {}'.format(extract_cfsr_bin, cfsr_file, output_cfsr_file, dirname(extract_cfsr_bin))
+        #cmd = 'sleep 0; touch {}'.format(output_cfsr_file) # DEBUG
 
-            try:
-                check_call(cmd, shell=True)
-                new_cfsr_files.append('{}.bin'.format(file))
-            except:
-                pass
+        try:
+            LOG.debug("cmd = \\\n\t{}".format(cmd.replace(' ',' \\\n\t')))
+            rc_extract_cfsr = 0
+            runscript(cmd, [delivery])
+        except CalledProcessError as err:
+            rc_extract_cfsr = err.returncode
+            LOG.error("extract_cfsr binary {} returned a value of {}".format(extract_cfsr_bin, rc_extract_cfsr))
+            return rc_extract_cfsr, []
 
-        return new_cfsr_files
+        # Verify output file
+        output_cfsr_file = glob(output_cfsr_file)
+        if len(output_cfsr_file) != 0:
+            output_cfsr_file = output_cfsr_file[0]
+            LOG.debug('Found flat CFSR file "{}"'.format(output_cfsr_file))
+        else:
+            LOG.error('Failed to generate "{}", aborting'.format(output_cfsr_file))
+            rc = 1
+            return rc, None
 
+        return rc, output_cfsr_file
 
-    def find_contexts(self, time_interval, sat, hirs_version, collo_version, csrb_version, ctp_version):
+    def hirs_to_time_interval(self, filename):
+        '''
+        Takes the HIRS filename as input and returns the time interval
+        covering that file.
+        '''
 
-        LOG.debug("Running find_contexts()")
-        files = delta_catalog.files('hirs', sat, 'HIR1B', time_interval)
-        return [{'granule': file.data_interval.left,
-                 'sat': sat,
-                 'hirs_version': hirs_version,
-                 'collo_version': collo_version,
-                 'csrb_version': csrb_version,
-                 'ctp_version': ctp_version}
-                for file in files
-                if file.data_interval.left >= time_interval.left]
+        file_chunks = filename.split('.')
+        begin_time = datetime.strptime('.'.join(file_chunks[3:5]), 'D%y%j.S%H%M')
+        end_time = datetime.strptime('.'.join([file_chunks[3], file_chunks[5]]), 'D%y%j.E%H%M')
 
-    def context_path(self, context, output):
+        return TimeInterval(begin_time, end_time)
 
-        LOG.debug("Running context_path()")
-        return os.path.join('HIRS',
-                            '{}/{}'.format(context['sat'], context['granule'].year),
-                            'CTP_ORBITAL')
+    def create_ctp_orbital(self, inputs, context):
+        '''
+        Create the CFSR statistics for the current day.
+        '''
+
+        rc = 0
+
+        # Create the output directory
+        current_dir = os.getcwd()
+
+        # Get the required CFSR and wgrib2 script locations
+        hirs_ctp_orbital_delivery_id = context['hirs_ctp_orbital_delivery_id']
+        delivery = delivered_software.lookup('hirs_ctp_orbital', delivery_id=hirs_ctp_orbital_delivery_id)
+        dist_root = pjoin(delivery.path, 'dist')
+        lut_dir = pjoin(dist_root, 'luts')
+        version = delivery.version
+
+        # Compile a dictionary of the input orbital data files
+        interval = self.hirs_to_time_interval(inputs['HIR1B'])
+        LOG.debug("HIRS interval {} -> {}".format(interval.left,interval.right))
+
+        # Determine the output filenames
+        output_file = 'hirs_ctp_orbital_{}_{}{}.nc'.format(context['satellite'],
+                                                          interval.left.strftime('D%y%j.S%H%M'),
+                                                          interval.right.strftime('.E%H%M'))
+        LOG.info("output_file: {}".format(output_file))
+
+        # Link the coefficient files into the working directory
+        shifted_coeffs = [abspath(x) for x in glob(pjoin(lut_dir,'shifted_hirs_FM_coeff/*'))]
+        unshifted_coeffs = [abspath(x) for x in glob(pjoin(lut_dir,'unshifted_hirs_FM_coeff/*'))]
+        linked_coeffs = link_files(current_dir, shifted_coeffs+
+                                   unshifted_coeffs+
+                                   [
+                                       abspath(pjoin(lut_dir, 'CFSR_lst.bin')),
+                                       abspath(pjoin(lut_dir, 'CO2_1979-2017_monthly_181_lat.dat'))
+                                   ])
+
+        LOG.debug("Linked coeffs: {}".format(linked_coeffs))
+
+        ctp_orbital_bin = pjoin(dist_root, 'bin/process_hirs_cfsr.exe')
+        debug = 0
+        shifted_FM_opt = 2
+
+        cmd = '{} {} {} {} {} {} {} {} {} {} {}'.format(
+                ctp_orbital_bin,
+                inputs['HIR1B'],
+                inputs['CFSR'],
+                inputs['COLLO'],
+                inputs['PTMSX'],
+                inputs['CSRB'],
+                'CFSR_lst.bin',
+                'CO2_1979-2017_monthly_181_lat.dat',
+                debug,
+                shifted_FM_opt,
+                output_file
+                )
+        #cmd = 'sleep 1; touch {}'.format(output_file) # DEBUG
+
+        try:
+            LOG.debug("cmd = \\\n\t{}".format(cmd.replace(' ',' \\\n\t')))
+            rc_ctp = 0
+            runscript(cmd, [delivery])
+        except CalledProcessError as err:
+            rc_ctp = err.returncode
+            LOG.error(" CTP orbital binary {} returned a value of {}".format(ctp_orbital_bin, rc_ctp))
+            return rc_ctp, None
+
+        # Verify output file
+        output_file = glob(output_file)
+        if len(output_file) != 0:
+            output_file = output_file[0]
+            LOG.debug('Found output  CTP orbital file "{}"'.format(output_file))
+        else:
+            LOG.error('Failed to generate "{}", aborting'.format(output_file))
+            rc = 1
+            return rc, None
+
+        return rc, output_file
+
+    @reraise_as(WorkflowNotReady, FileNotFound, prefix='HIRS_CTP_ORBITAL')
+    def run_task(self, inputs, context):
+        '''
+        Run the CTP Orbital binary on a single context
+        '''
+
+        LOG.debug("Running run_task()...")
+
+        for key in context.keys():
+            LOG.debug("run_task() context['{}'] = {}".format(key, context[key]))
+
+        rc = 0
+
+        # Extract a binary array from a CFSR reanalysis GRIB2 file on a
+        # global equal angle grid at 0.5 degree resolution. CFSR files
+        rc, cfsr_file = self.extract_bin_from_cfsr(inputs, context)
+
+        # Link the inputs into the working directory
+        inputs.pop('CFSR')
+        inputs = symlink_inputs_to_working_dir(inputs)
+        inputs['CFSR'] = cfsr_file
+
+        # Create the CTP Orbital for the current granule.
+        rc, ctp_orbital_file = self.create_ctp_orbital(inputs, context)
+
+        return {'out': ctp_orbital_file}
